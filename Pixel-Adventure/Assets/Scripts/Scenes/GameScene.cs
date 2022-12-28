a7dd6694e718ee4b76ded205b5726034f0cf3874
Contra613 <kim483123@gmail.com>
2022-12-28 16:42:00 +0900

    #1

--- Pixel-Adventure/Assets/Scripts/Scenes/GameScene.cs
+++ Pixel-Adventure/Assets/Scripts/Scenes/GameScene.cs
@@ -9,6 +9,9 @@ public class GameScene : BaseScene
         base.Init();
 
         SceneType = Define.Scene.Game;
+
+        Screen.SetResolution(1280, 720, false);     // 화면 비율
+
         //Managers.UI.ShowSceneUI<UI_Inven>();
         //Dictionary<int, Data.Stat> dict = Managers.Data.StatDict;
         //gameObject.GetOrAddComponent<CursorController>();
